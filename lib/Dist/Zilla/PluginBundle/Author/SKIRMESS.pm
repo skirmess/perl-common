564af10fa81844da6938015d629f25567320d18b
Sven Kirmess <sven.kirmess@kzone.ch>
2017-08-30 23:10:38 +0200

    0.007

--- lib/Dist/Zilla/PluginBundle/Author/SKIRMESS.pm
+++ lib/Dist/Zilla/PluginBundle/Author/SKIRMESS.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.006';
+our $VERSION = '0.007';
 
 use Moose 0.99;
 use namespace::autoclean 0.09;
--- lib/Dist/Zilla/Role/Author/SKIRMESS/Test/XT.pm
+++ lib/Dist/Zilla/Role/Author/SKIRMESS/Test/XT.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.006';
+our $VERSION = '0.007';
 
 use Moose::Role;
 with qw(
