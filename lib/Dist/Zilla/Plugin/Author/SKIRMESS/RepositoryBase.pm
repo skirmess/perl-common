ae81b2e3807c892a2e0289d58d2201bae79f4d52
Sven Kirmess <sven.kirmess@kzone.ch>
2017-10-19 00:22:08 +0200

    0.029

--- lib/Dist/Zilla/Plugin/Author/SKIRMESS/RepositoryBase.pm
+++ lib/Dist/Zilla/Plugin/Author/SKIRMESS/RepositoryBase.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.028';
+our $VERSION = '0.029';
 
 use Moose;
 
@@ -185,7 +185,7 @@ Dist::Zilla::Plugin::Author::SKIRMESS::RepositoryBase - Automatically create and
 
 =head1 VERSION
 
-Version 0.028
+Version 0.029
 
 =head1 SYNOPSIS
 
